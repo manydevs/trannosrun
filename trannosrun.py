4372ecc2134332eea8b6065982d07e91e5ca1a57
LabeledZed <81475174+LabeledZed@users.noreply.github.com>
2022-09-04 03:46:11 +0300

    Game launcher source code
    The final version packs this file into an executable in order to make Inno Setup work

---
+++ trannosrun.py
@@ -0,0 +1,23 @@
+import subprocess
+from contextlib import redirect_stdout
+import time
+import os
+import sys
+
+if getattr(sys, 'frozen', False):
+    truepath = os.path.dirname(sys.executable)
+else:
+    truepath = os.getcwd()
+
+with open(os.getcwd() + "\\trnsnd.bat", "x") as f:
+    with redirect_stdout(f):
+        print("@echo off\ncd scripts\n"
+              "start /b cmd /c " + str(truepath) + "\\env\\pythonw.exe " + str(truepath) + "\\scripts\\trmusic.pyw\n"
+              "start /b cmd /c " + str(truepath) + "\\env\\pythonw.exe " + str(truepath) + "\\scripts\\trgame.pyw")
+
+si = subprocess.STARTUPINFO()
+si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+subprocess.call(truepath + "\\trnsnd.bat", startupinfo=si)
+
+time.sleep(1)
+os.remove(truepath + "\\trnsnd.bat")
